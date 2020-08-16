b11e51181c3ce14bec8009516af71df483344927
Sam Gardiner <7697866+gardiners@users.noreply.github.com>
2020-08-16 18:04:32 +1000

    Spin script now renders everything in R/ except itself.

--- R/knit.R
+++ R/knit.R
@@ -2,7 +2,20 @@
 
 library(here)
 library(ezknitr)
+library(tidyverse)
 
-ezspin(file = here("R/eda.R"),
-       out_dir = here("output"),
-       keep_html = FALSE)
+# Scripts to render (don't render self!)
+scripts <- list.files(here("R"),
+                      pattern = r"(\.R$)",
+                      full.names = TRUE) %>%
+  discard(~str_ends(.x, "knit.R"))
+
+# Output directory
+output <- here("output")
+
+walk(scripts, function(script) {
+  ezspin(file = script,
+         out_dir = output,
+         keep_html = FALSE)
+})
+
