1fb9456229b39f935ec8603fe9e1a978e95503cf
Sam Gardiner <7697866+gardiners@users.noreply.github.com>
2020-08-19 13:51:28 +1000

    data_in now saves a plain tibble.

--- R/data_in.R
+++ R/data_in.R
@@ -27,9 +27,10 @@ nzl_raw <- read_spss(here("data", "WVS_Wave_7_New_Zealand_Spss_v1.4.sav"),
 #' Decide which of our questions are ordinal, on a 10-point scale (and
 #' therefore may end up being treated as continuous), continuous or nominal.
 q_numbers <- lst(
-  ordinal = c(1:6, 27:47, 51:55, 58:89, 113:118, 121, 131:138, 141:143, 146:148,
+  ordinal = c(1:6, 27:45, 47, 51:55, 58:89, 113:118, 121, 131:138, 141:143, 146:148,
               169:172, 196:199, 201:208, 221, 222, 224:239, 253, 255:259, 275:278, 287),
-  scale_10_point = c(49, 50, 90, 106:110, 112, 120, 158:164, 176:195, 240:252, 288),
+  scale_10_point = c(46, 49, 50, 90, 106:110, 112, 120, 158:164, 176:195, 240:252,
+                     288),
   continuous = c(261, 262),
   # These are sort-of ordinal but will need manual coding. They are currently
   # treated as categorical:
@@ -55,7 +56,9 @@ nzl_coded <- nzl_raw %>%
   drop_unused_value_labels() %>%
   # Covert labels to factors ready for analysis
   mutate(across(matches(q_names$ordinal), as_factor, ordered = TRUE),
-         across(matches(q_names$nominal), as_factor))
+         across(matches(q_names$nominal), as_factor)) %>%
+  # Remove label metadata so that result is a plain tibble/data frame.
+  zap_labels()
 
 #' # Data cleaning
 #' 
@@ -63,5 +66,3 @@ nzl_coded <- nzl_raw %>%
 
 #' # Serialise for other scripts
 saveRDS(nzl_coded, here("data", "nzl_coded.RDS"))
-
-
