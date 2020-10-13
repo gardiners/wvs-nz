a4cefafe7629c1d29c27f8dacdd827c81c9b3eb1
Sam Gardiner <7697866+gardiners@users.noreply.github.com>
2020-10-13 20:48:30 +1100

    Grahphical lasso to estimate sparse hetergenous correlation matrix.

--- R/cor.R
+++ R/cor.R
@@ -14,8 +14,10 @@ knitr::opts_chunk$set(echo = TRUE, message = FALSE, warn = FALSE, fig.width = 10
 #- init
 library(here)
 library(tidyverse)
-library(WGCNA)
 library(polycor)
+library(glasso)
+library(tidygraph)
+library(ggraph)
 
 windowsFonts("Arial Narrow" = windowsFont("Arial"))
 theme_set(theme_bw())
@@ -25,20 +27,8 @@ theme_rotate_x <- theme(axis.text.x = element_text(angle = -90,
 
 
 #' Load the data and select question variables.
-news_scores <- readRDS(here("data", "news_scores.RDS"))
-
 wvs_q <- readRDS(here("data", "nzl_coded.RDS")) %>%
-  select(H_URBRURAL, D_INTERVIEW,
-         matches(paste0("Q", c(1:260, 262, 269, 270, 274, 287, 288), "$"))) %>%
-  mutate(H_URBRURAL = factor(H_URBRURAL,
-                             levels = 1:2,
-                             labels = c("Urban", "Rural"))) %>%
-  rowwise() %>%
-  mutate(Mentions = sum(c(Q18, Q19, Q20, Q21, Q22, Q23, Q24, Q25, Q26) == 1,
-                        na.rm = TRUE)) %>%
-  ungroup() %>%
-  left_join(news_scores, by = "D_INTERVIEW") %>%
-  select(-D_INTERVIEW)
+  select(matches(paste0("Q", c(1:260, 262, 269, 270, 274, 287, 288), "$")))
 
 #' Compute heterogeneous correlation matrix. This is very computationally
 #' expensive, so cache the result as RDS.
@@ -49,3 +39,28 @@ if (file.exists(here("data", "cor_ext.RDS"))){
   wvs_q_cor <- hetcor(wvs_q, use = "pairwise")
   saveRDS(wvs_q_cor, here("data", "cor_ext.RDS"))
 }
+
+# Graphical lasso to estimate a sparse (regularised) correlation matrix
+
+wvs_q_cor_complete <- wvs_q_cor$correlations
+wvs_q_cor_complete[is.na(wvs_q_cor$correlations)] <- 0
+
+if (file.exists(here("data", "glasso.RDS"))){
+  wvs_glasso <- readRDS(here("data", "glasso.RDS"))
+} else {
+  wvs_glasso <- glasso(wvs_q_cor_complete,
+                       rho = 0.1,
+                       nobs = 1057)
+  saveRDS(wvs_glasso, here("data", "glasso.RDS"))
+}
+
+
+# Remove self-edges (matrix diagonals) and build network.
+
+glasso_net <- wvs_glasso$w %>%
+  `diag<-`(0) %>%
+  as_tbl_graph(directed = FALSE)
+
+
+ggraph(glasso_net, layout = "fr") +
+  geom_edge_link()
